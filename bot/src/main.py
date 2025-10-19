59582b1954b9ab0512e5e8ffb21e5568b8b90c29
Ivan-teapot <85389487+Ivan-teapot@users.noreply.github.com>
2025-10-19 14:42:14 +0300

    обновлённый ci, добавлена анотация ошибок форматов

--- bot/src/main.py
+++ bot/src/main.py
@@ -3,19 +3,19 @@ import os
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
-load_dotenv ()
+load_dotenv()
 
-BOT = os.getenv('BOT_TOKEN')
-bot = Bot(token=os.getenv('BOT_TOKEN'))
+BOT = os.getenv("BOT_TOKEN")
+bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher(bot)
 
-@dp.message_handler(command=['start'])
+@dp.message_handler(command=["start"])
 async def start_handler(message):
-    await message.answer('Бот запущен!')
+    await message.answer("Бот запущен!")
 
 async def main():
-    print('Bot_started')
+    print("Bot_started")
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
