1ca2f53569ccd2e1b0a4ca1534b9fa7ef582a1b2
Ivan-teapot <85389487+Ivan-teapot@users.noreply.github.com>
2025-10-19 15:09:53 +0300

    обновлённый ci, добавлена анотация ошибок форматов

--- bot/src/main.py
+++ bot/src/main.py
@@ -9,13 +9,16 @@ BOT = os.getenv("BOT_TOKEN")
 bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(command=["start"])
 async def start_handler(message):
     await message.answer("Бот запущен!")
 
+
 async def main():
     print("Bot_started")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
